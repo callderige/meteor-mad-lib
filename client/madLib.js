19a4d69a1dcd801905b394ab700b9289c8f90046
Darren <drrnyip@gmail.com>
2016-08-01 20:29:40 -0400

    added display text at bottom

--- client/madLib.js
+++ client/madLib.js
@@ -6,7 +6,8 @@ Template.title.events({
 		let noun = event.target.noun.value;
 		let verb = event.target.verb.value;
 
-		let madLib = "'" +  exclamation + "!' you say, as you " + adverb + " jump onto your " + noun + " and " +  verb + " into the sunset";
+		let madLib = "'" +  exclamation + "!' you say, as you " + adverb + " jump onto your " + noun + " and " +  verb + " into the sunset.";
+		event.target.display1.value = madLib;
 		console.log(madLib);
 	}
 });
