85a5960cc8ccc6a6354ab8f361f7859d252145e9
Darren <drrnyip@gmail.com>
2016-08-01 20:22:06 -0400

    console logged inputs

--- client/madLib.js
+++ client/madLib.js
@@ -1,7 +1,12 @@
 Template.title.events({
 	"submit .test"(event){
 		event.preventDefault();
-		console.log("hello world");
+		let exclamation = event.target.exclamation.value;
+		let adverb = event.target.adverb.value;
+		let noun = event.target.noun.value;
+		let verb = event.target.verb.value;
 
+		let madLib = "'" +  exclamation + "!' you say, as you " + adverb + " jump onto your " + noun + " and " +  verb + " into the sunset";
+		console.log(madLib);
 	}
 });
