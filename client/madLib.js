1549689bc599e74103008056f84706ee0cf4a161
Darren <drrnyip@gmail.com>
2016-08-01 22:13:35 -0400

    fixed bug

--- client/madLib.js
+++ client/madLib.js
@@ -7,7 +7,7 @@ Template.title.events({
 		let verb = event.target.verb.value;
 
 		let madLib = "'" +  exclamation + "!' you say, as you " + adverb + " jump onto your " + noun + " and " +  verb + " into the sunset.";
-		$("#title").hide();
+		$("#title1").hide();
 		$("#title1").fadeIn("slow").css("color", "#0000ff").text(madLib);
 		console.log(madLib);
 	}
